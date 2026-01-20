3bfc8e2654db4aacbf88981ad4fb5e5dbb296c27
sasikirankakara1 <sasikirankakaraprince@gmail.com>
2026-01-20 06:59:56 +0530

    new

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -5,5 +5,7 @@ describe('launch',()=>{
     })
     it.only('usage',async()=>{
         console.log("Title:",await browser.getTitle())
+        await browser.maximizeWindow()
+        await browser.pause(500)
     })
 })
